845f670bca6580e95b87a589278361c0053eaa84
Pui Yee Ng <hilarypyng@gmail.com>
2023-09-13 14:35:14 -0400

    Finished Lab2

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput,Button, StyleSheet, Switch } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function HelloWorldApp() {
+  const [value, onChangeText] = 	React.useState('');  
+  const [isSwitchOn, setIsSwitchOn] = useState("false");
+
+  const handleSwitchChange = () => {
+  setIsSwitchOn(!isSwitchOn);
+  };
+
+  const containerStyle = {
+    backgroundColor: isSwitchOn ? 'black' : 'white',
+    color: isSwitchOn ? 'white' : 'black',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  const editStyle = {
+    fontSize : 30,
+    borderColor: isSwitchOn ? 'white' : 'black',
+    borderWidth: 2,
+    marginBottom: 20,
+    color: isSwitchOn ? 'white' : 'black',
+    width: 300,
+    height: 40
+  };
+
+  const textStyle = {
+    fontSize : 35,
+    marginBottom: 20,
+    color: isSwitchOn ? 'white' : 'black',
+  };
+
+  const button = {
+    width: 200,
+    height: 50,
+    marginBottom: 20
+  }
+
+
+  return (  
+  <View style={containerStyle}>
+
+    <Text style= {textStyle}>Lab 2 by Pui Yee Ng</Text>
+
+    <TextInput style={editStyle}
+      placeholder= 'Enter your name here'
+	    onChangeText={text => onChangeText(text)}
+      clearTextOnFocus={true}
+      value={value}
+    />
+    
+    <Button style={button}
+       onPress={() => onChangeText('')}
+         title="Clear" 
+       />
+
+    <Switch
+        onChange={handleSwitchChange}
+        value= {isSwitchOn}
+      />
+
+   </View> 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
